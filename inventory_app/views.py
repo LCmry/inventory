8600c86f0d20bcbf53b3087b6068347e8e49077f
LCmry <lcrees.2014@gmail.com>
2016-03-17 17:20:05 -0700

    Created temp view method for index

--- inventory_app/views.py
+++ inventory_app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.template import Context, loader
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+  t = loader.get_template('inventory_app/index.html')
+  c = Context({ 'item': item, })
+  return HttpResponse(t.render(c))
