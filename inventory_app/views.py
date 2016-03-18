284250c6c0d244b15328704ecb509bbadb717b6b
LCmry <lcrees.2014@gmail.com>
2016-03-18 16:25:27 -0700

    Updated index view

--- inventory_app/views.py
+++ inventory_app/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 from django.template import Context, loader
 from django.http import HttpResponse
 
+from inventory_app.models import Item
+
 def index(request):
+  items = Item.objects.all()
   t = loader.get_template('inventory_app/index.html')
-  c = Context({ 'item': item, })
+  c = Context({ 'items': items, })
   return HttpResponse(t.render(c))
