7db4b6a8840767d80771fbdfbb0972be68249d4b
LCmry <lcrees.2014@gmail.com>
2016-03-19 18:57:56 -0700

    Added to admin

--- inventory_app/admin.py
+++ inventory_app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from inventory_app.models import Item, Category, Outlet
 
-# Register your models here.
+admin.site.register(Item)
+admin.site.register(Catgory)
+admin.site.register(Outlet)
