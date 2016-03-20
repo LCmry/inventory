0f157aebea3114c8c7365f072a9a24535925dd63
LCmry <lcrees.2014@gmail.com>
2016-03-19 18:56:23 -0700

    Added item, category, and outlet to models

--- inventory_app/models.py
+++ inventory_app/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class Item(models.Model):
+  name = models.CharField(max_length=100, blank=False)
+  last_modified = models.DateTimeField(auto_now=True)
+  total = models.IntegerField(default=0)
+  image = models.ImageField(upload_to="images", blank=True, null=True)
+  description = models.TextField()
+  category = models.ForeignKey(Category, on_delete=models.CASCADE)
+  outlets = models.ManyToManyField(Outlet)
+
+  def __str__(self):
+    return self.name
+
+class Category(models.Model):
+  name = models.CharField(max_length=50, blank=False)
+
+  def __str__(self):
+    return self.name
+
+class Outlet(models.Model):
+  name = models.CharField(max_length=50, blank=False)
+
+  def __str__(self):
+    return self.name
