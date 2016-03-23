f124c4f3c4af813073ee3a5de58a7efbd5a0d4ea
LCmry <lcrees.2014@gmail.com>
2016-03-23 16:38:12 -0700

    Added outlet and category templates

--- inventory_app/views.py
+++ inventory_app/views.py
@@ -2,10 +2,22 @@ from django.shortcuts import render
 from django.template import Context, loader
 from django.http import HttpResponse
 
-from inventory_app.models import Item
+from inventory_app.models import Item, Category, Outlet
 
 def index(request):
   items = Item.objects.all()
   t = loader.get_template('index.html')
   c = Context({ 'items': items, })
+  return HttpResponse(t.render(c))
+
+def categories(request):
+  categories = Category.objects.all().order_by('-name')
+  t = loader.get_template('catgories.html')
+  c = Context({ 'categories': categories, })
+  return HttpResponse(t.render(c))
+
+def outlets(request):
+  outlets = Outlet.objects.all().order_by('-name')
+  t = loader.get_template('outlets.html')
+  c = Context({ 'outlets': outlets, })
   return HttpResponse(t.render(c))
