416e05ed70691a4b9a152a1bdda635f218ddd661
LCmry <lcrees.2014@gmail.com>
2016-03-24 12:23:46 -0700

    Added item, category, outlet pages

--- inventory_app/urls.py
+++ inventory_app/urls.py
@@ -2,4 +2,9 @@ from django.conf.urls import url
 from inventory_app import views
 
 urlpatterns = [url(r'^$', views.index, name='index'),
+              url(r'^categories/', views.categories, name='categories'),
+              url(r'^outlets/', views.outlets, name='outlets'),
+              url(r'^item/(?P<id>[0-9]+)/$', views.item, name='item'),
+              url(r'^category/(?P<id>[0-9]+)/$', views.category, name='category'),
+              url(r'^outlet/(?P<id>[0-9]+)/$', views.outlet, name='outlet'),
               ]
--- inventory_app/views.py
+++ inventory_app/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.template import Context, loader
 from django.http import HttpResponse
 
-from inventory_app.models import Item, Category, Outlet
+from inventory_app.models import Item, Category, Outlet, Count
 
 def index(request):
   items = Item.objects.all()
@@ -11,13 +11,34 @@ def index(request):
   return HttpResponse(t.render(c))
 
 def categories(request):
-  categories = Category.objects.all().order_by('-name')
-  t = loader.get_template('catgories.html')
+  categories = Category.objects.all().order_by('name')
+  t = loader.get_template('categories.html')
   c = Context({ 'categories': categories, })
   return HttpResponse(t.render(c))
 
 def outlets(request):
-  outlets = Outlet.objects.all().order_by('-name')
+  outlets = Outlet.objects.all().order_by('name')
   t = loader.get_template('outlets.html')
   c = Context({ 'outlets': outlets, })
+  return HttpResponse(t.render(c))
+
+def category(request, id):
+  category = get_object_or_404(Category, id=id)
+  items = Item.objects.filter(category=id)
+  t = loader.get_template('category.html')
+  c = Context({ 'category': category, 'items': items })
+  return HttpResponse(t.render(c))
+
+def outlet(request, id):
+  outlet = get_object_or_404(Outlet, id=id)
+  counts = Count.objects.filter(outlet=id)
+  items = [c.item for c in counts]
+  t = loader.get_template('outlet.html')
+  c = Context({ 'outlet': outlet, 'items': items })
+  return HttpResponse(t.render(c))
+
+def item(request, id):
+  item = get_object_or_404(Item, id=id)
+  t = loader.get_template('item.html')
+  c = Context({ 'item': item })
   return HttpResponse(t.render(c))
