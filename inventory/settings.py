2bebd72bb71f4b2a1372bcee3944d7d55831cdec
LCmry <lcrees.2014@gmail.com>
2016-06-04 16:19:52 -0700

    changed debug

--- inventory/settings.py
+++ inventory/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'f$i+)_&2&27b7@yn6on-x#+%9-^2-hg^zvhownnl$=ynn*(7gp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
