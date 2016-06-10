1fd73ab90abeb3a705324352d5f7a760dd311efb
LCmry <lcrees.2014@gmail.com>
2016-06-10 10:57:51 -0700

    AWS S3

--- inventory/settings.py
+++ inventory/settings.py
@@ -35,6 +35,8 @@ if not DEBUG:
     import dj_database_url
     db_from_env = dj_database_url.config(conn_max_age=500)
     DATABASES['default'].update(db_from_env)
+    AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+    AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
     AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
     STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
