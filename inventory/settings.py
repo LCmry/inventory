6f6bcbfae2d64621ebc72f1539c622af940b6276
LCmry <lcrees.2014@gmail.com>
2016-06-10 14:24:55 -0700

    AWS S3

--- inventory/settings.py
+++ inventory/settings.py
@@ -35,10 +35,12 @@ if not DEBUG:
     import dj_database_url
     db_from_env = dj_database_url.config(conn_max_age=500)
     DATABASES['default'].update(db_from_env)
+    AWS_QUERYSTRING_AUTH = False
     AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
     AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
     AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
-    MEDIA_URL = 'http://%s.s3.amazonaws.com/images/' % (AWS_STORAGE_BUCKET_NAME)
+    MEDIA_FILE_LOCATION = 'images'
+    MEDIA_URL = 'https://%s.s3.amazonaws.com/%s/' % (AWS_STORAGE_BUCKET_NAME, MEDIA_FILE_LOCATION)
     STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
